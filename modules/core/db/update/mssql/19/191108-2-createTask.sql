498d3481bcebc97d77bf424d51633fba769026f7
Mario <mario@die-davids.de>
2019-11-08 09:05:27 +0100

    switch to MSSQL

---
+++ modules/core/db/update/mssql/19/191108-2-createTask.sql
@@ -0,0 +1,4 @@
+alter table CPBM_TASK add constraint FK_CPBM_TASK_ON_INITIATOR foreign key (INITIATOR_ID) references SEC_USER(ID);
+alter table CPBM_TASK add constraint FK_CPBM_TASK_ON_EXECUTOR foreign key (EXECUTOR_ID) references SEC_USER(ID);
+create index IDX_CPBM_TASK_ON_INITIATOR on CPBM_TASK (INITIATOR_ID);
+create index IDX_CPBM_TASK_ON_EXECUTOR on CPBM_TASK (EXECUTOR_ID);
